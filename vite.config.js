ad1d73fba6600c3eec7c0192dd1b174c1067f23c
rickjonathan2577-cloud <rickjonathan2577@gmail.com>
2025-12-08 21:49:37 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/Portofolio-/",
 })
 
-export default {
-  base: "/rickjonathan2577-cloud/Portofolio-"
-}
